a246ce082b26773a13e867c8debf90bebf4fb3f8
Andy Agus <redagus@gmail.com>
2020-05-22 02:25:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,7 +52,7 @@ monopoloy ={
         :mortgage_value => 200
       },
       :b_and_o_railroad => {
-        :mortgage_value => 100
+        :mortgage_value => 200
       },
       :shortline_railroad => {
         :mortgage_value => 100
